1f4a5ef2ac8ed03c6590e03f1b57b81d30b0f7d1
siplvijaygehlod <vijay.gehlod@systematixindia.com>
2019-02-14 15:30:56 +0530

    v2

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://api.unsplash.com',
-    headers:{
-        Authorization: 'Client-ID 511c78cc23bb0fc3dd16845221116a04c6f4aedbbfbb323eb0900d994380522e'
-    }
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization:
+      "Client-ID 511c78cc23bb0fc3dd16845221116a04c6f4aedbbfbb323eb0900d994380522e"
+  }
 });
